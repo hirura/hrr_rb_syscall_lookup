72b511baa0e2969bab0076a61bc485d0588c1bcf
hirura <hirura@gmail.com>
2020-02-29 16:44:00 +0900

    update to remove unused error class

--- lib/hrr_rb_syscall_lookup.rb
+++ lib/hrr_rb_syscall_lookup.rb
@@ -2,6 +2,4 @@ require "hrr_rb_syscall_lookup/version"
 require "hrr_rb_syscall_lookup/hrr_rb_syscall_lookup"
 
 module HrrRbSyscallLookup
-  class Error < StandardError; end
-  # Your code goes here...
 end
