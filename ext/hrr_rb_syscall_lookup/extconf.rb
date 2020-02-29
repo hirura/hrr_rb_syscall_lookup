cbbebf7718610cde280e47c1253a450e1eedda1b
hirura <hirura@gmail.com>
2020-02-29 20:49:38 +0900

    update extconf

--- ext/hrr_rb_syscall_lookup/extconf.rb
+++ ext/hrr_rb_syscall_lookup/extconf.rb
@@ -3,9 +3,6 @@ require "mkmf"
 if [
   have_header("libaudit.h"),
   have_library("audit"),
-  have_library("audit", "audit_name_to_machine"),
-  have_library("audit", "audit_name_to_syscall"),
-  have_library("audit", "audit_syscall_to_name"),
 ].all?
   create_makefile("hrr_rb_syscall_lookup/hrr_rb_syscall_lookup")
 end
