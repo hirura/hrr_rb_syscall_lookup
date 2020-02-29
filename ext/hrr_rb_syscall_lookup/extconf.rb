5283ff1b21f08ba1a249ecf6ff4946c8026440ba
hirura <hirura@gmail.com>
2020-02-29 16:43:31 +0900

    update extconf.rb to use libaudit

--- ext/hrr_rb_syscall_lookup/extconf.rb
+++ ext/hrr_rb_syscall_lookup/extconf.rb
@@ -1,3 +1,11 @@
 require "mkmf"
 
-create_makefile("hrr_rb_syscall_lookup/hrr_rb_syscall_lookup")
+if [
+  have_header("libaudit.h"),
+  have_library("audit"),
+  have_library("audit", "audit_name_to_machine"),
+  have_library("audit", "audit_name_to_syscall"),
+  have_library("audit", "audit_syscall_to_name"),
+].all?
+  create_makefile("hrr_rb_syscall_lookup/hrr_rb_syscall_lookup")
+end
